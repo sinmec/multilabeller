5ede05656b5a819c6b2e6cfaa5365b079107cd50
Higor0227 <higor0227@gmail.com>
2023-09-19 17:10:24 -0300

    Add zoom window

--- tkinter_tests/test.py
+++ tkinter_tests/test.py
@@ -14,7 +14,10 @@ class Aplicacao:
         self.height = None
         self.width = None
         self.channels = None
-
+        self.x1 = None
+        self.y1 = None
+        self.x2 = None
+        self.y2 = None
 
         self.janela = janela
         self.janela.title("Aplicacao OpenCV + Tkinter")
@@ -36,6 +39,25 @@ class Aplicacao:
         # Inicie a thread para executar o OpenCV
         self.iniciar_thread_opencv()
 
+    def criar_segunda_janela(self):
+        segunda_janela = tk.Toplevel(self.janela)
+
+        print('Estou abrindo uma janela')
+
+        self.segunda_janela = segunda_janela
+        self.segunda_janela.title('Zoom')
+        self.frame2 = tk.Frame(self.segunda_janela)
+
+        self.status_label_2 = tk.Label(self.segunda_janela, text="Status: Aguardando...")
+        self.status_label_2.pack()
+
+        # Crie uma área para exibir o vídeo (ou imagem) usando OpenCV
+        self.video_canvas_2 = tk.Canvas(self.segunda_janela, width=216, height=1024) #TODO: Implement dynamic canvas size
+        self.video_canvas_2.pack()
+
+
+
+
     def iniciar_thread_opencv(self):
         self.thread_opencv = threading.Thread(target=self.executar_opencv)
         self.thread_opencv.daemon = True
@@ -51,6 +73,7 @@ class Aplicacao:
         self.retangulo_y = self.mouse_y
         count = 1
         img_rectangle = img
+        self.criar_segunda_janela()
         while True:
             if self.wheel == 4:
                 self.retangulo_x = self.mouse_x
@@ -58,19 +81,12 @@ class Aplicacao:
                 count += 0.2
                 self.wheel = None
 
-                if (self.retangulo_x + (216 / count) / 2) > 216:
-                    self.retangulo_x = self.retangulo_x - ((self.retangulo_x + (216 / count) / 2) - 216)
-                if (self.retangulo_x - (216/count)/2) < 0:
-                    self.retangulo_x = self.retangulo_x + (0 - (self.retangulo_x - ((216/count) / 2)))
-
-                if (self.retangulo_y + (1024 / count) / 2) > 1024:
-                    self.retangulo_y = self.retangulo_y - ((self.retangulo_y + (1024 / count) / 2) - 1024)
-                if (self.retangulo_y - (1024/count)/2) < 0:
-                    self.retangulo_y = self.retangulo_y + (0 - (self.retangulo_y - ((1024/count) / 2)))
+                self.retangulo_x = self.check_borders(216, 1024, self.retangulo_x, self.retangulo_y, count)[0]
+                self.retangulo_y = self.check_borders(216, 1024, self.retangulo_x, self.retangulo_y, count)[1]
 
                 img_rectangle = self.criar_retangulo(img, self.retangulo_x, self.retangulo_y, 216 / count,
                                                          1024 / count)
-                self.atualizar_video(img_rectangle)
+                self.atualizar_video(img_rectangle, img)
             elif self.wheel == 5:
                 self.retangulo_x = self.mouse_x
                 self.retangulo_y = self.mouse_y
@@ -79,25 +95,18 @@ class Aplicacao:
                     count = 1
                 self.wheel = None
 
-                # if (self.retangulo_x + (216/count)/2) > 216:
-                #         self.retangulo_x = self.retangulo_x - (( self.retangulo_x + (216/count)/2) - 216)
-                # if (self.retangulo_x - (216/count)/2) < 0:
-                #     self.retangulo_x = self.retangulo_x + (0 - (self.retangulo_x - ((216/count) / 2)))
-                #
-                # if (self.retangulo_y + (1024 / count) / 2) > 1024:
-                #     self.retangulo_y = self.retangulo_y - ((self.retangulo_y + (1024 / count) / 2) - 1024)
-                # if (self.retangulo_y - (1024/count)/2) < 0:
-                #     self.retangulo_y = self.retangulo_y + (0 - (self.retangulo_y - ((1024/count) / 2)))
-
                 self.retangulo_x = self.check_borders(216, 1024, self.retangulo_x, self.retangulo_y, count)[0]
                 self.retangulo_y = self.check_borders(216, 1024, self.retangulo_x, self.retangulo_y, count)[1]
 
 
                 img_rectangle = self.criar_retangulo(img, self.retangulo_x, self.retangulo_y, 216 / count,
                                                          1024 / count)
-                self.atualizar_video(img_rectangle)
+                self.atualizar_video(img_rectangle, img)
             else:
-                self.atualizar_video(img_rectangle)
+                self.atualizar_video(img_rectangle, img)
+
+
+
 
     def check_borders(self, width, height, x, y, count):
         if (x + (width / count) / 2) > width:
@@ -121,59 +130,42 @@ class Aplicacao:
         x2 = int(x + largura / 2)
         y2 = int(y + altura / 2)
 
-        x1 = max(0, x1)
-        y1 = max(0, y1)
-        x2 = min(216, x2)
-        y2 = min(1024, y2)
+        self.x1 = max(0, x1)
+        self.y1 = max(0, y1)
+        self.x2 = min(216, x2)
+        self.y2 = min(1024, y2)
 
-        print(x1, y1, x2, y2)
-        cv2.rectangle(imagem_com_retangulo, (x1, y1), (x2, y2), cor, espessura)
+        cv2.rectangle(imagem_com_retangulo, (self.x1, self.y1), (self.x2, self.y2), cor, espessura)
 
         return imagem_com_retangulo
 
 
-    def aplicar_zoom(self, frame, zoom):
-        altura, largura, _ = frame.shape
-
-        # Calcule as novas dimensões da imagem com base no zoom
-        nova_altura = int(altura * zoom)
-        nova_largura = int(largura * zoom)
-
-        # if zoom < 1.01:
-        #     return frame
-
-        # Calcule as coordenadas do canto superior esquerdo da ROI
-        x1 = int(self.mouse_x - nova_largura / 2)
-        y1 = int(self.mouse_y - nova_altura / 2)
-
-        # Garanta que a ROI esteja dentro dos limites da imagem
-        x1 = max(0, x1)
-        y1 = max(0, y1)
-        x2 = min(largura, x1 + nova_largura)
-        y2 = min(altura, y1 + nova_altura)
-
-        # Recorte a ROI da imagem original
-        roi = frame[y1:y2, x1:x2]
-
-        # Redimensione a ROI
-        frame_zoom = cv2.resize(roi, (largura, altura))
-
-        return frame_zoom
-
-    def atualizar_video(self, frame):
+    def atualizar_video(self, frame_rectangle, frame):
         # Converte o frame do OpenCV para um formato que o Tkinter possa exibir
-        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        frame_rgb = cv2.cvtColor(frame_rectangle, cv2.COLOR_BGR2RGB)
         imagem = tk.PhotoImage(data=cv2.imencode('.png', frame_rgb)[1].tobytes())
 
         # Atualize o canvas do Tkinter
         self.video_canvas.create_image(0, 0, anchor=tk.NW, image=imagem)
         self.video_canvas.imagem = imagem  # Mantém uma referência para evitar a coleta de lixo
 
+        frame_rgb_2 = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        frame_rgb_zoom = frame_rgb_2[self.y1:self.y2, self.x1:self.x2]
+
+        resize = cv2.resize(frame_rgb_zoom, (216, 1024))
+
+        image_2 = tk.PhotoImage(data=cv2.imencode('.png', resize)[1].tobytes())
+
+        self.video_canvas_2.create_image(0, 0, anchor=tk.NW, image=image_2)
+        self.video_canvas_2.imagem = image_2  # Mantém uma referência para evitar a coleta de lixo
+
+
+
     def on_mouse_wheel(self, event):
-        if event.num == 4:
-            print('zoom para cima')
-        elif event.num == 5:
-            print('zoom para baixo')
+        #if event.num == 4:
+            #print('zoom para cima')
+        #elif event.num == 5:
+            #print('zoom para baixo')
 
         self.wheel = event.num
 
@@ -182,7 +174,7 @@ class Aplicacao:
 
         self.mouse_x = event.x
         self.mouse_y = event.y
-        print(self.mouse_x, self.mouse_y)
+        #print(self.mouse_x, self.mouse_y)
 
 
 
@@ -196,8 +188,5 @@ if __name__ == "__main__":
     app.video_canvas.bind("<Button-5>", app.on_mouse_wheel)
     app.video_canvas.bind("<Motion>", app.on_mouse_motion)
     app.video_canvas.bind("<Button-1>", app.on_mouse_wheel)
-
-
-
 
     root.mainloop()
